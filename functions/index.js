d1ee2ba723ab86767babeab859473345938c6509
cjp666 <chris.j.podmore@gmail.com>
2018-02-15 18:24:57 +0000

    updated to read firestore and return details of either a previous or the next ride

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,18 @@
 'use strict';
 
 // https://github.com/actions-on-google/dialogflow-facts-about-google-nodejs/blob/master/functions/index.js
+// https://firebase.google.com/docs/firestore/query-data/get-data
 
 const { DialogflowApp } = require('actions-on-google');
 const functions = require('firebase-functions');
 
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
 const RIDE_INTENT = 'ride';
+
+const d = new Date();
+const date = new Date(d.getFullYear(), d.getMonth(), d.getDate(), 0, 0, 0, 0);
 
 let parameters = {};
 
@@ -30,28 +37,84 @@ function findRide(assistant) {
         rideDay = 'next';
     }
 
-    switch(rideDay.toLowerCase()) {
+    switch (rideDay.toLowerCase()) {
         case 'next':
             return nextRide(assistant);
         case 'previous':
-            return previousRide(assistant);
+            previousRide(assistant);
+            break;
         case 'todays':
-            return todaysRide(assistant);
+            todaysRide(assistant);
+            break;
         case 'tomorrows':
-            return tomorrowsRide(assistant);
+            tomorrowsRide(assistant);
+            break;
         default:
-            assistant.tell('I do not understand when you want to know about');
+            return assistant.tell('I do not understand when you want to know about');
     }
 }
 
 function nextRide(assistant) {
-    console.log('nextRide');
-    assistant.tell('There is no next ride');
+    return admin.firestore().collection('rides')
+        .where('date', '>=', date)
+        .orderBy('date')
+        .limit(1)
+        .get()
+        .then(rides => {
+            if (rides.size === 0) {
+                return assistant.tell('I am sorry but I am unable to locate the details of the next ride, you might need to check the website or Facebook');
+            }
+            let message = 'Unable to locate the next ride';
+            rides.forEach(ride => {
+                const data = ride.data();
+                if (data.isEvent) {
+                    message = data.description;
+                } else {
+                    const rideDate = new Date(data.date);
+                    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+                    message = `The next ride is level ${data.level} on ${rideDate.toLocaleDateString('en-gb', options)},`
+                        + ` being lead by ${data.rideLeader} and will be leaving from ${data.start}`
+                        + ` heading for ${data.lunch}`;
+                }
+            });
+            return assistant.tell(message);
+        })
+        .catch(error => {
+            console.error(error);
+            throw error;
+        });
 }
 
 function previousRide(assistant) {
-    console.log('previousRide');
-    assistant.tell('There is no previous ride');
+    return admin.firestore().collection('rides')
+        .where('date', '<', date)
+        .orderBy('date', 'desc')
+        .limit(1)
+        .get()
+        .then(rides => {
+            console.log(rides.size);
+            if (rides.size === 0) {
+                return assistant.tell('I am sorry but I am unable to locate the details of the last ride, you might need to check the website or Facebook');
+            }
+            let message = 'Unable to locate the last ride';
+            rides.forEach(ride => {
+                const data = ride.data();
+                if (data.isEvent) {
+                    message = data.description;
+                } else {
+                    const rideDate = new Date(data.date);
+                    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+                    message = `The last ride was ${rideDate.toLocaleDateString('en-gb', options)}`
+                        + ` from ${data.start} and was lead by ${data.rideLeader}`
+                        + ` with lunch at ${data.lunch}`;
+                }
+            });
+            return assistant.tell(message);
+        })
+        .catch(error => {
+            console.error(error);
+            throw error;
+        });
 }
 
 function todaysRide(assistant) {
