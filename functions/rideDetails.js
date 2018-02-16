2e17b0ad42a410eaf03a4566009ac1121db186b1
cjp666 <chris.j.podmore@gmail.com>
2018-02-16 19:13:53 +0000

    #10 - attempting to get code coverage to work from build

--- functions/rideDetails.js
+++ functions/rideDetails.js
@@ -4,4 +4,12 @@ const format = function(rideData) {
     return 'testing';
 }
 
-module.exports = { format };
+const doSomething = function() {
+    return 'test 2';
+}
+
+function doSomething2() {
+    return 'test 3';
+}
+
+module.exports = { format, doSomething };
