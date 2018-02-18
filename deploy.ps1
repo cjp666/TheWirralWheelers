cd0699c8f7853b170f6af9551afe10cb7bed1300
cjp666 <chris.j.podmore@gmail.com>
2018-02-18 14:24:28 +0000

    #17 deploy functions if on master branch

--- deploy.ps1
+++ deploy.ps1
@@ -1,15 +1,18 @@
-Write-Host 'Running AppVeyor deployment script' -ForegroundColor Yellow
+Write-Host "Running AppVeyor deployment script" -ForegroundColor Yellow
 Write-Host "Build version : $env:APPVEYOR_BUILD_VERSION"
 Write-Host "Author        : $env:APPVEYOR_REPO_COMMIT_AUTHOR"
 Write-Host "Branch        : $env:APPVEYOR_REPO_BRANCH"
 
 # we only want to deploy if on the master branch
-if ($env:APPVEYOR_REPO_BRANCH -notmatch 'master')
+if ($env:APPVEYOR_REPO_BRANCH -notmatch "master")
 {
     Write-Host "Not deploying branch: $env:APPVEYOR_REPO_BRANCH - Exiting"
     exit;
 }
 
-firebase deploy --only functions --token $FIREBASE_TOKEN
-
-Write-Host 'Deployment complete'
+firebase deploy --only functions --token 1/RhaFJsg4XQM3Cmnx8QHsvN2H1YzEQJyT_MNLgRGT0so
+if ($lastexitcode -notmatch 0) 
+{
+    throw "Deployment Failed"
+}
+Write-Host "Deployment complete"
